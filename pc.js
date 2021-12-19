0c785782da7c04d244ed2443f65a97390a4c2267
shark-has-crash <oskargrahamtaylor@gmail.com>
2021-12-19 01:06:30 +0000

    .

--- pc.js
+++ pc.js
@@ -3,4 +3,4 @@ var root_domain = temp[1] + '.' + temp[0];
 
 if (root_domain != "sharkhascrash.com" && root_domain != "lvh.me") {
     window.location.replace("https://www.youtube.com/watch?v=yPYZpwSpKmA");
-} 
+}
